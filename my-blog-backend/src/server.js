d01be67a9cc5ebb0b33318a8e36dc5c2ba5caff2
jin <s3736719@student.rmit.edu.au>
2020-06-29 16:30:51 +1000

    Integrate endpoints with db

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -3,22 +3,27 @@ import bodyParser from "body-parser";
 import { MongoClient } from "mongodb";
 
 const app = express();
-const withDB = async (operations) => {
+// Use the BODY PARSER to parse the JSON object in the REQUEST and add a BODY property to the REQUEST parameter
+app.use(bodyParser.json());
+// After that, we can use req.body.var to access JSON
+
+// Before connecting to DB, make sure mongo db is up running
+// brew services start mongodb-community@4.2
+// brew services stop mongodb-community@4.2
+const withDB = async (operations, res) => {
   try {
     const client = await MongoClient.connect("mongodb://localhost:27017", {
-      userNewUrlParser: true,
+      useNewUrlParser: true,
     });
     const db = client.db("my-blog");
+
     await operations(db);
+
     client.close();
   } catch (error) {
     res.status(500).json({ message: "Error connection to db!", error });
   }
 };
-
-// Use the BODY PARSER to parse the JSON object in the REQUEST and add a BODY property to the REQUEST parameter
-app.use(bodyParser.json());
-// After that, we can use req.body.var to access JSON
 
 // Set a GET endpoint
 app.get("/api/articles/:name", async (req, res) => {
@@ -30,7 +35,7 @@ app.get("/api/articles/:name", async (req, res) => {
 
     // Send JSON response
     res.status(200).json(articleInfo);
-  });
+  }, res);
 });
 
 // Set a POST endpoint(use URL params)
@@ -57,17 +62,39 @@ app.post("/api/articles/:name/upvote", async (req, res) => {
 
     // Send JSON response
     res.status(200).json(updatedInfo);
-  });
+  }, res);
 });
 
 // Set a POST endpoint(use body parser)
-app.post("/api/articles/:name/add-comment", (req, res) => {
+app.post("/api/articles/:name/add-comment", async (req, res) => {
   const articleName = req.params.name;
   const { username, text } = req.body;
 
-  articleInfo[articleName].comments.push({ username, text });
+  withDB(async (db) => {
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
 
-  res.status(200).send(articleInfo[articleName]);
+    await db.collection("articles").updateOne(
+      { name: articleName },
+      // {
+      //   $set: {
+      //     comments: articleInfo.comments.concat({ username, text }),
+      //   },
+      // }
+      {
+        $push: {
+          comments: { username: username, text: text },
+        },
+      }
+    );
+
+    const updatedInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    res.status(200).json(updatedInfo);
+  }, res);
 });
 
 // Start the server
